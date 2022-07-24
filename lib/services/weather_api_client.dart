037eabfdb546a8048584537724441ecf481df91b
Wahni Adnani <75670541+defrein@users.noreply.github.com>
2022-07-25 01:37:40 +0700

    Add files via upload

---
+++ lib/services/weather_api_client.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/model/weather_model.dart';
+
+class WeatherApiClient {
+  Future<Weather>? getCurrentWeather(String? location) async {
+    var endpoint = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=5a61648f93caf8206d47135ed28928ec&units=metric");
+
+    var response = await http.get(endpoint);
+    var body = jsonDecode(response.body);
+    print(Weather.fromJson(body).cityName);
+    return Weather.fromJson(body);
+  }
+}
